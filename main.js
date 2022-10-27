931784384e50d7322aa7e6026bcb6e984b07c46b
SaverioCascelli <saverio.cascelli@gmail.com>
2022-10-27 16:03:22 +0200

    post linke to js for base elements

--- main.js
+++ main.js
@@ -55,3 +55,45 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById("container");
+
+printInHtml();
+
+function printInHtml(){
+    posts.forEach((content,i) =>{
+
+        container.innerHTML +=`
+        <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${content.author.image}" alt="${content.name}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${content.author.name}</div>
+                            <div class="post-meta__time">TODO</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${content.content}</div>
+                <div class="post__image">
+                    <img src="${content.media}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${content.likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>` 
+    });
+          
+}
